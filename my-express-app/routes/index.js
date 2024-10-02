d1bdd7c1ed32e5b6e191059f3fbbbdbb4f095687
tribuanaayu <tribuanaayu72@gmail.com>
2024-10-02 17:48:24 +0700

    Latihan - Contact
    ini adalah latihan membuat halaman contact us - Tribuana Ayu

--- my-express-app/routes/index.js
+++ my-express-app/routes/index.js
@@ -8,4 +8,19 @@ router.get('/', function(req, res, next) {
 router.get("/about", function(req, res, next){
   res.render('about', {title: 'About Us'});
 });
+
+router.get('/contact', function (req, res, next) {
+  res.render('contact', {
+    title: 'Contact Us!'
+  });
+});
+
+router.post('/submit-contact', (req, res, next)=> {
+  const {name} = req.body;
+  res.send (`
+    <h1> Thank you, ${name} </h1>
+    <p> Your message has been received, we will get back to you soon!</p>
+    <a href="/contact"> Back to Contact </a>
+    `);
+});
 module.exports = router;
