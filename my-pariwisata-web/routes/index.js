1ef1079e6fa0cc4975fbb7db737ccd4b53dc4520
tribuanaayu <tribuanaayu72@gmail.com>
2024-10-04 18:36:53 +0700

    Latihan Wisata 2 - listpaketwisata
    Bismillah dapet point UTS

--- my-pariwisata-web/routes/index.js
+++ my-pariwisata-web/routes/index.js
@@ -3,11 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Beranda' });
 });
 
 router.get('/paketwisata', function(req, res, next) {
-  res.render('paketwisata', {title: "Paket Wisata"});
+let listpaketwisata =[
+  {'nama' : 'Wisata Pulau Kemaro', 'harga' : 500000},
+  {'nama' : 'Wisata Punti Kayu', 'harga' : 250000},
+  {'nama' : 'Wisata Jakabaring', 'harga' : 100000},
+]
+
+  res.render('paketwisata', {
+    title: 'Paket Wisata', 
+    listpaketwisata: listpaketwisata
+  });
 });
 
 router.get('/orderpaket', function(req, res, next) {
