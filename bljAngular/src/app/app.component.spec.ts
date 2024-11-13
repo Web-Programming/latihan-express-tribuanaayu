573a9db8040dfa400b4bbd08265e4e5b8c9b6935
JkOmxr <143261728+JkOmxr@users.noreply.github.com>
2024-11-13 20:19:58 +0700

    1

--- bljAngular/src/app/app.component.spec.ts
+++ bljAngular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'belajar_Angular' title`, () => {
+  it(`should have the 'bljAngular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('belajar_Angular');
+    expect(app.title).toEqual('bljAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, belajar_Angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bljAngular');
   });
 });
